030fa663903c500d970b9d4a7a0147ee185e32cb
Jake Kenny <jakenny@asu.edu>
2022-04-20 10:24:53 -0700

    Added Submit button for previous updates
    PLEASE DO NOT CLICK IT AS THE PROGRAM WILL CRASH
    We need to program the button to do as its label says

--- user_interface.py
+++ user_interface.py
@@ -111,11 +111,11 @@ def create_library_UI():
         2. Set the 'values' parameter equal to the SQL database
     """
     library_layout = [
-        [sg.Text("Add song to Favorites: "), sg.InputText()],
+        [sg.Text("Add song to Favorites: "), sg.InputText(), sg.Button("Submit")],
         [sg.Table(values = [['0', '0']], headings = ['0', '1'])]
         ]
     favorites_layout = [
-        [sg.Text("Remove song from Favorites: "), sg.InputText()],
+        [sg.Text("Remove song from Favorites: "), sg.InputText(), sg.Button("Submit")],
         [sg.Table(values = [['0', '0']], headings = ['0', '1'])]
         ]
     tabgrp = [
